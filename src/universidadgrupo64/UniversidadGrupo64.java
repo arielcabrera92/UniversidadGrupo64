8afa8031f31e5b7fa78bf953d9cea605c26ba6bd
Ariel Mauricio Cabrera <arielcabrera.mza@gmail.com>
2023-09-14 15:18:23 -0300

    prueba de alumno y base de datos

--- src/universidadgrupo64/UniversidadGrupo64.java
+++ src/universidadgrupo64/UniversidadGrupo64.java
@@ -15,9 +15,13 @@ public class UniversidadGrupo64 {
     public static void main(String[] args) {
         // TODO code application logic here
         //Connection con = Conexion.getConexion();
-        Alumno juan=new Alumno(12312311,"Luna","Pedro",LocalDate.of(1980, 4, 25),true);
+        /*Alumno juan=new Alumno(12312311,"Luna","Pedro",LocalDate.of(1980, 4, 25),true);
         AlumnoData alu=new AlumnoData();
         alu.guardarAlumno(juan);
+        */
+        Alumno martin =new Alumno(34569874,"Funes","Martin",LocalDate.of(1980, 4, 25),true);
+        AlumnoData alu=new AlumnoData();
+        alu.guardarAlumno(martin);
         
         
     }
